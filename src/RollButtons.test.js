1831587ab48ee8525053a61780b48421f37bd65a
Justin Yum <justinyum98@gmail.com>
2018-09-25 17:28:05 -0700

    added two unit tests for Roll and RollButtons

---
+++ src/RollButtons.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import RollButtons from './RollButtons';
+import Roll from './Roll';
+import { shallow } from 'enzyme';
+
+it('should render 11 Roll components', () => {
+  const wrapper = shallow(<RollButtons />);
+  const rolls = wrapper.find(Roll);
+  
+  expect(rolls.length).toEqual(11);
+});
