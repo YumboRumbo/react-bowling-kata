fec5ee90346a57b0eccb6b9a06c68640e7674264
Justin Yum <justinyum98@gmail.com>
2018-12-07 17:26:20 -0800

    fixed tests, all green

--- src/components/Frame/Frame.test.js
+++ src/components/Frame/Frame.test.js
@@ -2,54 +2,42 @@ import React from "react";
 import Frame from "./Frame"
 import { shallow } from "enzyme";
 
-it('should display first and second roll along with total score', () => {
+it('should display first and second roll along with frame score', () => {
   const rolls = [5, 4];
+  const expectedFrameScore = 25;
   
-  const wrapper = shallow(<Frame rolls={rolls} />);
+  const wrapper = shallow(<Frame rolls={rolls} frameScore={expectedFrameScore} />);
   const firstRoll = wrapper.find('#firstRoll').props().children;
   const secondRoll = wrapper.find('#secondRoll').props().children;
-  const totalScore = wrapper.find('#totalScore').props().children;
+  const actualFrameScore = wrapper.find('#frameScore').props().children;
 
   expect(firstRoll).toEqual(rolls[0]);
   expect(secondRoll).toEqual(rolls[1]);
-  expect(totalScore).toEqual(rolls[0] + rolls[1]);
+  expect(actualFrameScore).toEqual(expectedFrameScore);
 });
 
 it('should display blank if rolls are empty', () => {
   const rolls = [];
 
-  const wrapper = shallow(<Frame rolls={rolls} />);
+  const wrapper = shallow(<Frame rolls={rolls} frameScore={undefined} />);
   const firstRoll = wrapper.find('#firstRoll').props().children;
   const secondRoll = wrapper.find('#secondRoll').props().children;
-  const totalScore = wrapper.find('#totalScore').props().children;
+  const frameScore = wrapper.find('#frameScore').props().children;
 
   expect(firstRoll).toEqual('');
   expect(secondRoll).toEqual('');
-  expect(totalScore).toEqual('');
-});
-
-it('should display blank total score if one roll is empty', () => {
-  const rolls = [5];
-
-  const wrapper = shallow(<Frame rolls={rolls} />);
-  const firstRoll = wrapper.find('#firstRoll').props().children;
-  const secondRoll = wrapper.find('#secondRoll').props().children;
-  const totalScore = wrapper.find('#totalScore').props().children;
-  
-  expect(firstRoll).toEqual(5);
-  expect(secondRoll).toEqual('');
-  expect(totalScore).toEqual('');
+  expect(frameScore).toEqual('');
 });
 
 it('should display blank if rolls array is undefined', () => {
   const wrapper = shallow(<Frame />);
   const firstRoll = wrapper.find('#firstRoll').props().children;
   const secondRoll = wrapper.find('#secondRoll').props().children;
-  const totalScore = wrapper.find('#totalScore').props().children;
+  const frameScore = wrapper.find('#frameScore').props().children;
 
   expect(firstRoll).toEqual('');
   expect(secondRoll).toEqual('');
-  expect(totalScore).toEqual('');
+  expect(frameScore).toEqual('');
 });
 
 it('should display spares as /', () => {
